b6672e92f03fd0d5d2ac0569a25e8d063fbf3b77
Phil Martin <root@frillip.com>
2019-09-30 11:36:48 +0100

    Added bme280 and some JSON

--- megaio-web.py
+++ megaio-web.py
@@ -1,5 +1,35 @@
 from aiohttp import web
 import megaio
+import bme280
+import smbus2
+import json
+
+bme_port = 1 # Yes
+bme_address = 0x76 # Cheap BME280s are 0x76, Adafruit is 0x77
+bme_bus = smbus2.SMBus(bme_port)
+
+bme280.load_calibration_params(bme_bus,bme_address)
+
+def get_environment_json():
+    global bme_bus
+    global bme_address
+    bme_json_data = {}
+    print("Getting BME data... ",end = '')
+    try:
+        bme280_data = bme280.sample(bme_bus,bme_address)
+        print("Success!")
+        bme_json_data['h'] = round(bme280_data.humidity,1)
+        bme_json_data['p'] = round(bme280_data.pressure)
+        bme_json_data['t'] = round(bme280_data.temperature,1)
+        bme_json_data['e'] = False
+    except:
+        print("Fail!")
+        bme_json_data['h'] = 0
+        bme_json_data['p'] = 0
+        bme_json_data['t'] = 0
+        bme_json_data['e'] = True
+    bme_json = json.dumps(bme_json_data)
+    return bme_json
 
 megaio_stack_id = 0
 
@@ -55,6 +85,24 @@ async def buttonhandler(request):
             return web.Response(text="Lighting now ON")
     return web.FileResponse('./index.html')
 
+async def status_json(request):
+    if megaio.get_relays(megaio_stack_id) & fence_relay_mask == fence_on_value:
+        json_fence_status = True
+    else:
+        json_fence_status = False
+    if megaio.get_relays(megaio_stack_id) & cameras_relay_mask == cameras_on_value:
+        json_cameras_status = True
+    else:
+        json_cameras_status = False
+    if megaio.get_relays(megaio_stack_id) & lighting_relay_mask == lighting_on_value:
+        json_lighting_status = True
+    else:
+        json_lighting_status = False
+
+async def bme_data(request):
+    resp = get_environment_json()
+    return web.Response(text=resp,content_type='application/json')
+
 async def fence_status(request):
     if megaio.get_relays(megaio_stack_id) & fence_relay_mask == fence_on_value:
         fence_status_text="ON"
@@ -79,6 +127,7 @@ async def lighting_status(request):
 app = web.Application(debug=True)
 app.add_routes([web.get('/', indexresp),
 		web.post('/', buttonhandler),
+                web.get('/bme.json', bme_data),
 		web.get('/fence_status', fence_status),
 		web.get('/cameras_status', cameras_status),
 		web.get('/lighting_status', lighting_status)])
