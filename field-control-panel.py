ffd05effaf0f8886a64d1976233132fb4bf25a96
Phil Martin <root@frillip.com>
2019-10-18 19:40:42 +0100

    Modem stats now available via json

--- field-control-panel.py
+++ field-control-panel.py
@@ -3,6 +3,7 @@ import json
 from bme_env_data import get_bme_data
 from megaio_get_data import get_relay_data,get_batt_data,get_pv_data
 from megaio_set_relays import set_relay_state
+from e3372_interface import get_modem_data
 
 async def indexresp(request):
     return web.FileResponse('./static/index.html')
@@ -18,6 +19,7 @@ async def status_json(request):
     status_data['bme'] = get_bme_data()
     status_data['batt'] = get_batt_data()
     status_data['pv'] = get_pv_data()
+    status_data['modem'] = get_modem_data()
     resp_json = json.dumps(status_data)
     return web.Response(text=resp_json,content_type='application/json')
 
@@ -37,6 +39,10 @@ async def pv_json(request):
     resp_json = json.dumps(get_pv_data())
     return web.Response(text=resp_json,content_type='application/json')
 
+async def modem_json(request):
+    resp_json = json.dumps(get_modem_data())
+    return web.Response(text=resp_json,content_type='application/json')
+
 app = web.Application()
 app.add_routes([web.get('/', indexresp),
 		web.post('/', buttonhandler),
@@ -44,5 +50,6 @@ app.add_routes([web.get('/', indexresp),
                 web.get('/relay.json', relay_json),
                 web.get('/bme.json', bme_json),
 		web.get('/batt.json', batt_json),
-		web.get('/pv.json', pv_json)])
+		web.get('/pv.json', pv_json),
+		web.get('/modem.json', modem_json)])
 web.run_app(app)
