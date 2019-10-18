0a17c9f603d15021b8cde7100e1b741f42bda2d1
Phil Martin <root@frillip.com>
2019-10-18 22:56:25 +0100

    Can now read data from Victron MPPT controller and return via JSON/dict. Fixes #10

--- field-control-panel.py
+++ field-control-panel.py
@@ -1,9 +1,10 @@
 from aiohttp import web
 import json
 from bme_env_data import get_bme_data
-from megaio_get_data import get_relay_data,get_batt_data,get_pv_data
+from megaio_get_data import get_relay_data
 from megaio_set_relays import set_relay_state
 from e3372_interface import get_modem_data
+from vedirect_interface import get_mppt_data
 
 async def indexresp(request):
     return web.FileResponse('./static/index.html')
@@ -17,26 +18,21 @@ async def status_json(request):
     status_data = {}
     status_data['relay'] = get_relay_data()
     status_data['bme'] = get_bme_data()
-    status_data['batt'] = get_batt_data()
-    status_data['pv'] = get_pv_data()
+    status_data['mppt'] = get_mppt_data()
     status_data['modem'] = get_modem_data()
     resp_json = json.dumps(status_data)
-    return web.Response(text=resp_json,content_type='application/json')
-
-async def bme_json(request):
-    resp_json = json.dumps(get_bme_data())
     return web.Response(text=resp_json,content_type='application/json')
 
 async def relay_json(request):
     resp_json = json.dumps(get_relay_data())
     return web.Response(text=resp_json,content_type='application/json')
 
-async def batt_json(request):
-    resp_json = json.dumps(get_batt_data())
+async def bme_json(request):
+    resp_json = json.dumps(get_bme_data())
     return web.Response(text=resp_json,content_type='application/json')
 
-async def pv_json(request):
-    resp_json = json.dumps(get_pv_data())
+async def mppt_json(request):
+    resp_json = json.dumps(get_mppt_data())
     return web.Response(text=resp_json,content_type='application/json')
 
 async def modem_json(request):
@@ -49,7 +45,6 @@ app.add_routes([web.get('/', indexresp),
                 web.get('/status.json', status_json),
                 web.get('/relay.json', relay_json),
                 web.get('/bme.json', bme_json),
-		web.get('/batt.json', batt_json),
-		web.get('/pv.json', pv_json),
+		web.get('/mppt.json', mppt_json),
 		web.get('/modem.json', modem_json)])
 web.run_app(app)
