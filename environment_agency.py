f8f16d51a632d1c8904fb3b8f7a3dcdde5a25de2
Phil Martin <root@frillip.com>
2019-11-21 11:45:51 +0000

    Added river status. Fixes #43

--- environment_agency.py
+++ environment_agency.py
@@ -15,6 +15,7 @@ logger.setLevel(logging.INFO)
 
 env_agency_api_url = "https://environment.data.gov.uk/flood-monitoring/id/measures/4195-level-stage-i-15_min-mASD"
 env_agency_api_station_url = "https://environment.data.gov.uk/flood-monitoring/id/stations/4195"
+river_level_previous = 0.0
 river_level_sent = 0.0
 
 def init_river():
@@ -24,6 +25,7 @@ def init_river():
         global_vars.river_data["id"] = resp.json()["items"]["stationReference"]
         global_vars.river_data["name"] = resp.json()["items"]["riverName"]
         global_vars.river_data["level"] = resp.json()["items"]["measures"]["latestReading"]["value"]
+        global_vars.river_data["status"] = "steady"
         global_vars.river_data["last_reading"] = resp.json()["items"]["measures"]["latestReading"]["dateTime"][:-1]
         global_vars.river_data["last_high_level"] = 0.0
         global_vars.river_data["high"] = 0.95
@@ -47,17 +49,29 @@ def init_river():
 def check_river():
 
     global env_agency_api_url
+    global river_level_previous
     global river_level_sent
 
     try:
+        river_level_previous = global_vars.river_data["level"]
         resp = requests.get(env_agency_api_url)
         global_vars.river_data["level"] = resp.json()["items"]["latestReading"]["value"]
+
+        if global_vars.river_data["level"] > river_level_previous:
+            global_vars.river_data["status"] = "rising"
+        elif global_vars.river_data["level"] < river_level_previous:
+            global_vars.river_data["status"] = "falling"
+        else:
+            global_vars.river_data["status"] = "steady"
+
         global_vars.river_data["last_reading"] = resp.json()["items"]["latestReading"]["dateTime"][:-1]
+
         human_datetime = datetime.now().strftime("%d/%m/%Y %H:%M")
         now_iso_stamp = datetime.now().replace(microsecond=0).isoformat()
         if global_vars.river_data["level"] > global_vars.river_data["last_high_level"]:
             global_vars.river_data["last_high_level"] = global_vars.river_data["level"]
             global_vars.river_data["last_high"] = now_iso_stamp
+
         if global_vars.river_data["level"] > global_vars.river_data["high_warn"]:
             if not global_vars.river_data["warning_active"] or ( global_vars.river_data["warning_active"] and global_vars.river_data["level"] > ( river_level_sent + 0.1) ):
                 if not global_vars.river_data["warning_active"]:
