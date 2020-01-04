2f4208f7d163066428c234f7a3792e978d4a0d6f
Phil Martin <root@frillip.com>
2020-01-04 18:33:25 +0000

    Relay map and change timestamp dict now generated on load of global_vars.py. Fixes #78

--- global_vars.py
+++ global_vars.py
@@ -38,11 +38,9 @@ modem_data = {}
 modem_data["data_usage"] = {}
 river_data = {}
 
-relay_map = { "fence" : 1 , "cameras" : 2, "lighting" : 3 }
-relay_timestamp = { 1 : 0 , 2: 0 , 3 : 0}
-
 relay_data = {}
 relay_data[1] = {}
+relay_data[1]['enabled'] = True
 relay_data[1]['name'] = "fence"
 relay_data[1]['raw_state'] = False
 relay_data[1]['invert'] = True
@@ -53,6 +51,7 @@ relay_data[1]['auto_timeout'] = 0
 relay_data[1]['last_state_change'] = datetime.now().replace(microsecond=0).isoformat()
 
 relay_data[2] = {}
+relay_data[2]['enabled'] = True
 relay_data[2]['name'] = "cameras"
 relay_data[2]['raw_state'] = False
 relay_data[2]['invert'] = False
@@ -63,6 +62,7 @@ relay_data[2]['auto_timeout'] = 300
 relay_data[2]['last_state_change'] = datetime.now().replace(microsecond=0).isoformat()
 
 relay_data[3] = {}
+relay_data[3]['enabled'] = True
 relay_data[3]['name'] = "lighting"
 relay_data[3]['raw_state'] = False
 relay_data[3]['invert'] = False
@@ -71,3 +71,12 @@ relay_data[3]['auto_on'] = False
 relay_data[3]['auto_off'] = False
 relay_data[3]['auto_timeout'] = 0
 relay_data[3]['last_state_change'] = datetime.now().replace(microsecond=0).isoformat()
+
+relay_map = {}
+relay_timestamp = {}
+
+for relay_number in relay_data:
+    if relay_data[relay_number]['enabled']:
+        relay_name = relay_data[relay_number]['name']
+        relay_map[relay_name] = relay_number
+        relay_timestamp[relay_number] = 0
